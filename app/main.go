e349041bcf8fe39a3bc3dbf6d84250920155eb2b
nagaremono <ediguruhdev@gmail.com>
2025-03-26 03:00:00 +0700

    feat: handle command input error

--- app/main.go
+++ app/main.go
@@ -9,5 +9,11 @@ import (
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println(os.Stderr, "Error reading command:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
