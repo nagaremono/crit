a47a358aa68c20f593e4e5f8ef3e840e9b33eff0
nagaremono <ediguruhdev@gmail.com>
2025-03-26 04:24:57 +0700

    feat: support exit command

--- app/main.go
+++ app/main.go
@@ -4,17 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Println(os.Stderr, "Error reading command:", err)
 			os.Exit(1)
 		}
 
-		fmt.Println(command[:len(command)-1] + ": command not found")
+		input = strings.Trim(input, "\n")
+		command := strings.Split(input, " ")
+
+		if command[0] != "exit" {
+			fmt.Println(command[0] + ": command not found")
+		}
+
+		if len(command) >= 2 == false {
+			continue
+		}
+
+		exitCode, err := strconv.Atoi(command[1])
+		if err != nil {
+			fmt.Println(os.Stderr, "Invalid codee", err)
+			os.Exit(1)
+		}
+
+		os.Exit(exitCode)
 	}
 }
