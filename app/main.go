41f07c5ffbdc160c61be24439a7eb7a6a9e692b7
nagaremono <ediguruhdev@gmail.com>
2025-03-27 10:25:12 +0700

    refactor: move commands back to single file

--- app/main.go
+++ app/main.go
@@ -4,6 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -34,4 +38,69 @@ func main() {
 			run(commandName, commandArgs)
 		}
 	}
+}
+
+func exit(commandArgs []string) {
+	if len(commandArgs) >= 1 == false {
+		return
+	}
+
+	exitCode, err := strconv.Atoi(commandArgs[1])
+	if err != nil {
+		fmt.Println(os.Stderr, "Invalid codee", err)
+		os.Exit(1)
+	}
+
+	os.Exit(exitCode)
+}
+
+func echo(commandArgs []string) {
+	if !(len(commandArgs) >= 2) {
+		fmt.Println("")
+	}
+
+	toPrint := commandArgs[1:]
+
+	fmt.Println(strings.Join(toPrint, " "))
+}
+
+func typeOf(commandArgs []string) {
+	if !(len(commandArgs) >= 2) {
+		fmt.Println(": not found")
+	}
+	toCheck := commandArgs[1]
+
+	if slices.Contains(builtIns, toCheck) {
+		fmt.Println(toCheck + " is a shell builtin")
+		return
+	}
+
+	path := os.Getenv("PATH")
+	dirs := strings.Split(path, ":")
+
+	for _, dir := range dirs {
+		location := filepath.Join(dir, toCheck)
+		_, err := os.Stat(location)
+		if err == nil {
+			fmt.Fprintf(os.Stdout, "%s is %s\n", toCheck, location)
+			return
+		}
+	}
+
+	fmt.Fprintf(os.Stdout, "%s: not found\n", toCheck)
+}
+
+func run(commandName string, commandArgs []string) {
+	_, err := exec.LookPath(commandName)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "%s: not found\n", commandName)
+		return
+	}
+
+	cmd := exec.Command(commandName, commandArgs...)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+	}
+	fmt.Fprintf(os.Stdout, "%s", out)
 }
